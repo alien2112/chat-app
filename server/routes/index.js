44b9173aa4c762577eb99ae1a14f314ae49e692f
Eslam Abdullatif <105902657+alien2112@users.noreply.github.com>
2024-07-12 13:25:23 +0300

    Update index.js
    adding favorite user routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -24,6 +24,10 @@ router.get("/logout", logout);
 router.post("/update-user", updateUserDetails);
 //search users
 router.post("/search-user", searchUser)
+///////
+router.post("/add-favorite", addFavorite);
+router.get("/favorites", getFavorites);
+
 router.get("/hello",(req,res)=>{
          return res.status(200).json({
             message:"hello from vercel",
